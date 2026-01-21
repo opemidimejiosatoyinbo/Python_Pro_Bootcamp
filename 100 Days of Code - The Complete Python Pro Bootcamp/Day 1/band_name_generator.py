796298dbd4bf1b011d075173e2c0ed51657514a3
Opemidimeji OSATOYINBO <opemidimejiosatoyinbo@gmail.com>
2026-01-21 16:48:13 +0100

    Add files via upload

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/band_name_generator.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/band_name_generator.py
@@ -1,10 +1,10 @@
-# This code generates a Band name for the user
+# This code generates a Band name for the user by:
 """
-1. Create a greeting for your program.
-2. Ask the user for the city that they grew up in and store it in a variable.
-3. Ask the user for the name of a pet and store it in a variable.
-4. Combine the name of their city and pet and show them their band name.
-5. Make sure the input cursor shows on a new line:
+1. Writing a greeting for the User.
+2. Asking the user for the city that they grew up in and storing it in a variable.
+3. Asking the user for the name of a pet and storing it in a variable.
+4. Combining the name of their city and pet and showing them their band name.
+5. Also making sure the input cursor shows on a new line:
 """
 print("Welcome to the Band Name Generator.")
 name_of_city = input("Which city did you grow up in?\n")
