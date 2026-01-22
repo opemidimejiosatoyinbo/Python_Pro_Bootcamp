e479c7b7a8862611d952fad8b839bfab326fbd48
Opemidimeji OSATOYINBO <opemidimejiosatoyinbo@gmail.com>
2026-01-22 12:52:39 +0100

    Add files via upload

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/tip_calculator.py
@@ -0,0 +1,16 @@
+# This code builds a tip calculator.
+"""
+We're going to build a tip calculator:
+Indicate the bill, the number of people to split with, and the percentage of tip.
+The result indicate the amount each person should pay, formatting the result to 2 decimal places.
+"""
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+print(f"Each person should pay: ${final_amount}")
